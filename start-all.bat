d842f6bb01538df790e2a5f987de0082f81c2f01
Krishna Singh Shahi <63533035+krishnasinghshahi@users.noreply.github.com>
2024-09-27 14:02:56 +0530

    Add change to start-all.bat

--- start-all.bat
+++ start-all.bat
@@ -1,144 +1,84 @@
 @echo off
 setlocal enabledelayedexpansion
 
+REM Check for required programs
+where git >nul 2>&1 || (echo Git is not installed. && exit /b)
+where npm >nul 2>&1 || (echo NPM is not installed. && exit /b)
+where node >nul 2>&1 || (echo Node.js is not installed. && exit /b)
+where python >nul 2>&1 || (echo Python is not installed. && exit /b)
+
 :menu
 echo Choose an option:
-echo 1. Install all (Recommended for first time setup)
-echo 2. Update, install and run (Recommended when a update is available)
-echo 3. Run existing version
-echo 4. Exit
-set /p choice="Enter your choice (1, 2, 3, or 4): "
+echo 1. Install/Update (Recommended once in a while)
+echo 2. Run existing version
+echo 3. Exit
+set /p choice="Enter your choice (1, 2, or 3): "
 
 if "%choice%"=="1" goto install
-if "%choice%"=="2" goto update_and_run
-if "%choice%"=="3" goto run
-if "%choice%"=="4" goto end
+if "%choice%"=="2" goto run
+if "%choice%"=="3" goto end
 echo Invalid choice. Please try again.
 goto menu
 
 :install
+REM Update the entire monorepo
+echo Updating steadfast-monorepo...
+git pull || (echo Failed to update steadfast-monorepo. && exit /b)
+
 REM Clone the app repository and install dependencies
 echo Checking app repository...
 if not exist "steadfast-app\.git" (
     echo Cloning app...
-    git clone https://github.com/narenkram/steadfast-app.git
-) else (
-    echo App repository already exists, skipping clone...
-)
-cd steadfast-app
+    git clone https://github.com/krishnasinghshahi/steadfast-app.git
+) 
+cd steadfast-app || (echo Failed to change directory to steadfast-app. && exit /b)
+echo Updating steadfast-app...
+git pull || (echo Failed to update steadfast-app. && exit /b)
 echo Installing app dependencies...
-call npm install
-if !errorlevel! neq 0 (
-    echo Error occurred while installing app dependencies.
-    goto :error
-)
+call npm install && call npm audit fix || (echo Failed to install app dependencies. && exit /b)
 cd ..
 
 REM Clone the API repository and install dependencies
 echo Checking API repository...
 if not exist "steadfast-api\.git" (
     echo Cloning API...
-    git clone https://github.com/narenkram/steadfast-api.git
-) else (
-    echo API repository already exists, skipping clone...
-)
-cd steadfast-api
+    git clone https://github.com/krishnasinghshahi/steadfast-api.git
+) 
+cd steadfast-api || (echo Failed to change directory to steadfast-api. && exit /b)
+echo Updating steadfast-api...
+git pull || (echo Failed to update steadfast-api. && exit /b)
 echo Installing API dependencies...
-call npm install
-if !errorlevel! neq 0 (
-    echo Error occurred while installing API dependencies.
-    goto :error
-)
+call npm install && call npm audit fix || (echo Failed to install API dependencies. && exit /b)
 cd ..
 
 REM Clone the WebSocket repository and install dependencies
 echo Checking WebSocket repository...
 if not exist "steadfast-websocket\.git" (
     echo Cloning WebSocket...
-    git clone https://github.com/narenkram/steadfast-websocket.git
-) else (
-    echo WebSocket repository already exists, skipping clone...
+    git clone https://github.com/krishnasinghshahi/steadfast-websocket.git
 )
-cd steadfast-websocket
+cd steadfast-websocket || (echo Failed to change directory to steadfast-websocket. && exit /b)
+echo Updating steadfast-webSocket...
+git pull || (echo Failed to update steadfast-websocket. && exit /b)
 
 echo Installing WebSocket...
 
 REM Install NorenRestApi without dependencies for Flattrade and Shoonya
 echo Installing NorenRestApi for Flattrade and Shoonya...
-pip install --no-deps NorenRestApi
-if !errorlevel! neq 0 (
-    echo Error occurred while installing NorenRestApi.
-    goto :error
-)
+pip install --no-deps NorenRestApi  || (echo Failed to install NorenRestApi. && exit /b)
 
 echo Installing Flattrade dependencies...
-cd flattrade
-call pip install -r requirements.txt
-if !errorlevel! neq 0 (
-    echo Error occurred while installing WebSocket dependencies for Flattrade.
-    goto :error
-)
+cd flattrade || (echo Failed to change directory to flattrade. && exit /b)
+pip install -r requirements.txt || (echo Failed to install Flattrade dependencies. && exit /b)
 cd ..
 
 echo Installing Shoonya dependencies...
-cd shoonya
-call pip install -r requirements.txt
-if !errorlevel! neq 0 (
-    echo Error occurred while installing WebSocket dependencies for Shoonya.
-    goto :error
-)
+cd shoonya || (echo Failed to change directory to shoonya. && exit /b)
+pip install -r requirements.txt || (echo Failed to install Shoonya dependencies. && exit /b)
 cd ..\..
 
-echo Repositories cloned and dependencies installed successfully.
+echo Repositories and dependencies installed/updated successfully.
 goto menu
-
-:update_and_run
-REM Update the entire monorepo
-echo Updating steadfast-monorepo...
-git pull https://github.com/narenkram/steadfast-monorepo main
-if !errorlevel! neq 0 (
-    echo Error updating steadfast-monorepo.
-    goto :error
-)
-
-REM Update steadfast-app
-echo Updating steadfast-app...
-cd steadfast-app
-git pull https://github.com/narenkram/steadfast-app main
-if !errorlevel! neq 0 (
-    echo Error updating steadfast-app.
-    goto :error
-)
-echo Installing app dependencies...
-call npm install
-call npm audit fix
-cd ..
-
-REM Update steadfast-api
-echo Updating steadfast-api...
-cd steadfast-api
-git pull https://github.com/narenkram/steadfast-api main
-if !errorlevel! neq 0 (
-    echo Error updating steadfast-api.
-    goto :error
-)
-echo Installing api dependencies...
-call npm install
-call npm audit fix
-cd ..
-
-REM Update steadfast-websocket
-echo Updating steadfast-websocket...
-cd steadfast-websocket
-git pull https://github.com/narenkram/steadfast-websocket main
-if !errorlevel! neq 0 (
-    echo Error updating steadfast-websocket.
-    goto :error
-)
-cd ..
-
-echo Update complete.
-goto run
 
 :run
 REM Start the API in a new command prompt window
@@ -182,4 +122,6 @@ echo All services stopped.
 goto menu
 
 :end
+echo Exiting the script. Goodbye!
 endlocal
+exit /b
